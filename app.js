c3f8639ff5e1bb02127c04a561d67f6fd701ac1d
FillipeF5 <fillipefreitas220@gmail.com>
2021-04-19 22:04:31 -0300

    second commit

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const express = require ('express');
 const app = express();
+app.use (express.json());
 
 // http://localhost:3000/
 app.get ('/', (req,res) => {
     res.send('Olá Mundo!!!');
 });
 
-let usuarios = {nome: 'Fillipe', sobrenome: 'F. Monteiro'};
+let usuarios = [{nome: 'Fillipe', sobrenome: 'F. Monteiro'}];
 
 app.get('/usuarios', (req,res) => {
     res.json(usuarios);
 });
+
+app.post('/usuarios', (req,res) =>{
+    const {nome, sobrenome} = req.body;
+    usuarios.push({ nome, sobrenome });
+    res.json ({nome, sobrenome});
+});
+
+app.put('/usuarios/:nome', (req,res) => {
+    const {nome} = req.params;
+    const {sobrenome} = req.body;
+})
 
 app.listen(3000, () => {
     console.log('O servidor está rodando...');
